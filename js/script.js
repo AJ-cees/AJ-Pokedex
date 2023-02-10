28afc23b9f514a263d4d4d80a2d1349c405fcf3c
AJ-cees <aj.observer09@gmail.com>
2023-02-10 14:44:53 -0500

    movieList addition
    Previous favoriteFood code remoied. Added movieList variable containing an array of spooky films.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-alert('Hello world');
-let favoriteFood = 'Tiramasu';
-document.write(favoriteFood);
+let movieList = [
+    {title:'Scream', year: '1996',  type: ['slasher','American']},
+    {title:'Friday the 13th', year: '1980', type: ['slasher', 'American']},
+    {title: 'Nightmare on Elm Street', year: '1984', type: ['slasher', 'American']},
+    {title: 'Train to Busan', year: '2016', type: ['zombie', 'South Korean']},
+    {title: 'The Call', year: '2020', type: ['time-travel', 'South Korean']},
+    {title: 'As the Gods Will', year: '2014', type:['supernatural','Japanese']}
+
+];
