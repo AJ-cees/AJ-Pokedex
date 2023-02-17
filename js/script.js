89a6277a2fbceedecb67cba96667512ad967e973
AJ-cees <aj.observer09@gmail.com>
2023-02-17 16:52:38 -0500

    IIFE addition
    Added movieRepository IIFE

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-let movieList = [
+// Wraps movieList in IIFE
+let movieRepository = (function() {
+  let movieList = [
     {title:'Scream', year: '1996',  type: ['slasher','American']},
     {title:'Friday the 13th', year: '1980', type: ['slasher', 'American']},
     {title: 'Nightmare on Elm Street', year: '1984', type: ['slasher', 'American']},
@@ -7,9 +9,29 @@ let movieList = [
     {title: 'As the Gods Will', year: '2014', type:['supernatural','Japanese']}
 ];
 
-//Prints movieList array on the page
+function add(movie) {
+  movieList.push (movie);
+}
+
+function getAll() {
+  return movieList;
+}
+
+return {
+  add: add,
+  getAll: getAll
+};
+
+})();
+
+//Prints movie array
+console.log(movieRepository.getAll());
+movieRepository.add({name: 'The Blob'});
+console.log(movieRepository.getAll());
 
 function movieLoop(movie) {
   console.log (movie.title + ' is a ' + movie.type [0] + ' type of movie ');
+  document.write (movie.title + ' is a ' + movie.type [0] + ' type of movie ');
 }
-movieList.forEach(movieLoop);
+
+movieRepository.forEach(getAll);
